e7cb7152e4d5097ef81d96b41b4c29488371b7a4
kevinwidjayaa <65111750+kevinwidjayaa@users.noreply.github.com>
2020-10-17 09:02:20 +0700

    coba

--- src/app/tab1/list-barang.service.ts
+++ src/app/tab1/list-barang.service.ts
@@ -5,27 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ListBarangService {
-private dt:DT[]=[
-       {
-        id:'br1',
-        foto:'https://tweakers.net/i/6mFqDEsyG7j6arsjLSC4e6re2ZI=/i/2000645311.jpeg',
-        merek:'kingston',
-        speed:'3200mhz',
-        ukuran:'16gb',
-        harga:'10.000',
-        stok:'30',
-       
-	   },
-       {
-        id:'br2',
-        foto:'https://tweakers.net/i/6mFqDEsyG7j6arsjLSC4e6re2ZI=/i/2000645311.jpeg',
-        merek:'vgen',
-        speed:'4000mhz',
-        ukuran:'16gb',
-        harga:'20.000',
-        stok:'40',
-	   }
-   ];
+
  
   constructor() { }
  
